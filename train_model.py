7ee03bc2cbeeb485c9f3126babdfae74b2d0fdeb
chandana1710 <chandana.vs18@gmail.com>
2026-05-28 22:04:30 +0530

    Optimize model size for Render free tier

--- train_model.py
+++ train_model.py
@@ -24,7 +24,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Create model
-model = RandomForestRegressor()
+model = RandomForestRegressor(  n_estimators=50,   # was default 100
+    max_depth=10,      # was unlimited
+    random_state=42
+)
 
 # Train
 model.fit(X_train, y_train)
